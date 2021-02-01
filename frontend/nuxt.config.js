69646c88b1e300af7235d9781258fb595ed283da
ushakc <34007594+ushakc@users.noreply.github.com>
2021-01-31 22:00:03 -0600

    Adding UIKit css framework to vuejs frontend

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -17,10 +17,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "uikit/dist/css/uikit.min.css",
+    "uikit/dist/css/uikit.css",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/uikit.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
