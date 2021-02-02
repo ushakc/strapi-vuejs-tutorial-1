dc52748ffd58c7066053574ba7e30d02835c4b7b
ushakc <34007594+ushakc@users.noreply.github.com>
2021-02-02 16:05:06 -0600

    Calling a "restaurantsQuery" GraphQL query with Apollo  Creating a folder with requests that are  made to the Strapi API

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,13 +1,10 @@
-
 <template>
   <div>
-    // You call your new Header component
     <Header />
 
     <div class="uk-section uk-section-default">
       <div class="uk-container uk-container-large">
 
-        // This is where all yours pages will be
         <nuxt />
 
       </div>
@@ -17,7 +14,6 @@
 </template>
 
 <script>
-// Import your new Header component
 import Header from '~/components/Header.vue'
 export default {
   components: {
